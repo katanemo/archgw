b228f152f7c8b354559a1c50d6692ac93c5bd0a7
Musa <malikmusa1323@gmail.com>
2025-12-23 14:22:13 -0800

    feat(docs): observability updated + logo inversion issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,6 +114,7 @@ html_theme_options = asdict(theme_options)
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 pygments_style = "lovelace"
 pygments_style_dark = "github-dark"
